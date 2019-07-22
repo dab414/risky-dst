2c9f85e411caa019dc136106ca15b56c10f3ad10
Dave <dab414@lehigh.edu>
2019-07-22 12:33:00 -0400

    Modification to downloadAndProcess.sh

--- exp1/dataProcessing/downloadAndProcess.sh
+++ exp1/dataProcessing/downloadAndProcess.sh
@@ -1,4 +1,6 @@
 scp -P 18765 davebrau@ns1.us69.siteground.us:~/public_html/turk/data/dissertation/dissertationExperiment1/*.txt ./txtData/pilot/real/
 ./dataProcessing.py txtData/pilot/**/*.txt
-Rscript --vanilla /home/dave/OneDrive/Research/By\ Project/Dissertation/experiments/analysis/exp1/scripts/preprocessing/knitAnalysis.r
+Rscript --vanilla /home/dave/OneDrive/Research/By\ Project/Dissertation/experiments/analysis/exp1/scripts/knitAnalysis.r
 firefox /home/dave/OneDrive/Research/By\ Project/Dissertation/experiments/analysis/exp1/scripts/preprocessing/index.html
+firefox /home/dave/OneDrive/Research/By\ Project/Dissertation/experiments/analysis/exp1/scripts/confirmatory/choice/index.html
+firefox /home/dave/OneDrive/Research/By\ Project/Dissertation/experiments/analysis/exp1/scripts/confirmatory/performance/index.html
