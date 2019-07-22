d2d7ad1537fb67a3e5f6280137491193f3d30b65
Dave <dab414@lehigh.edu>
2019-07-21 22:00:38 -0400

    Work on Experiment 1 data pipeline

--- exp1/dataProcessing/downloadAndProcess.sh
+++ exp1/dataProcessing/downloadAndProcess.sh
@@ -1,2 +1,4 @@
-scp -P 18765 davebrau@ns1.us69.siteground.us:~/public_html/turk/data/dissertation/dissertationExperiment1/*.txt ./txtData
-./dataProcessing.py txtData/*.txt
+scp -P 18765 davebrau@ns1.us69.siteground.us:~/public_html/turk/data/dissertation/dissertationExperiment1/*.txt ./txtData/pilot/real/
+./dataProcessing.py txtData/pilot/**/*.txt
+Rscript --vanilla /home/dave/OneDrive/Research/By\ Project/Dissertation/experiments/analysis/exp1/scripts/preprocessing/knitAnalysis.r
+firefox /home/dave/OneDrive/Research/By\ Project/Dissertation/experiments/analysis/exp1/scripts/preprocessing/index.html
