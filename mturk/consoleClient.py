234d04537adeb780d6caa7a2ac75ba6854bbfb68
Dave <dab414@lehigh.edu>
2019-07-23 21:28:00 -0400

    Update general journal

--- mturk/consoleClient.py
+++ mturk/consoleClient.py
@@ -1,17 +1,17 @@
 
-	import sys
-	sys.path.append('/home/dave/OneDrive/Research/By Project/Dissertation/experiments/private/')
-	from awsKeys import aws_access_key_id
-	from awsKeys import aws_secret_access_key
-	import boto3
+import sys
+sys.path.append('/home/dave/OneDrive/Research/By Project/Dissertation/experiments/private/')
+from awsKeys import aws_access_key_id
+from awsKeys import aws_secret_access_key
+import boto3
 
-	region_name = 'us-east-1'
-
-
-	HOST = 'https://mturk-requester.us-east-1.amazonaws.com'
+region_name = 'us-east-1'
 
 
-	mtc = boto3.client('mturk', endpoint_url = HOST,region_name = region_name,aws_access_key_id = aws_access_key_id,aws_secret_access_key = aws_secret_access_key)
+HOST = 'https://mturk-requester.us-east-1.amazonaws.com'
+
+
+mtc = boto3.client('mturk', endpoint_url = HOST,region_name = region_name,aws_access_key_id = aws_access_key_id,aws_secret_access_key = aws_secret_access_key)
 
 
 
