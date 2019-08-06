1474109fd467d288125ef0166986aba65faedad9
Dave <dab414@lehigh.edu>
2019-08-06 17:20:25 -0400

    Release Exp 3 pilot and prepare Exp 3 production.

--- mturk/scripts/createHit.py
+++ mturk/scripts/createHit.py
@@ -11,11 +11,10 @@ HOST = 'https://mturk-requester-sandbox.us-east-1.amazonaws.com' # Use this to p
 #HOST = 'https://mturk-requester.us-east-1.amazonaws.com'
 
 pay = '1.5'
-max_assignments = 1
+max_assignments = 9
 lifetime = 2 * 24 * 60 * 60 # days, hours, mins, seconds
 
-
-exp = 'exp2'
+exp = 'exp3'
 
 def PostHits(pay, lifetime, max_assignments, exp):
 
@@ -24,15 +23,17 @@ def PostHits(pay, lifetime, max_assignments, exp):
 	keywords = 'attention, psychology, experiment, research'
 	title = 'A Decision Making Experiment'
 	experimentName = 'Decision Making Experiment' ## this is NOT what it ends up getting called on my server
-	description = 'This HIT will take about 30 mins to complete. All HITS in this batch are the same, and you will only be able to perform one of the HITS in this batch.'
+	description = 'This HIT will take about 10 mins to complete. All HITS in this batch are the same, and you will only be able to perform one of the HITS in this batch.'
 
-	#q = ExternalQuestion(external_url = "https://davebraun.org/dissertation/experiments/production/" + exp +"/", frame_height=675)
+
+  ## PRODUCTION LINK
+	q = ExternalQuestion(external_url = "https://davebraun.org/dissertation/experiments/production/" + exp +"/", frame_height=675)
 
 	## MAKE UP HIT LINK
 	#q = ExternalQuestion(external_url = "https://davebraun.org/dissertation/experiments/production/makeupHit", frame_height=675)
 
 	## EXP 3 PILOT
-	q = ExternalQuestion(external_url = "https://davebraun.org/dissertation/experiments/production/pilots/exp3Pilot/", frame_height=675)
+	#q = ExternalQuestion(external_url = "https://davebraun.org/dissertation/experiments/production/pilots/exp3Pilot/", frame_height=675)
 
 	qr = [{
     'QualificationTypeId': '000000000000000000L0',
@@ -65,7 +66,7 @@ def PostHits(pay, lifetime, max_assignments, exp):
 													Description = description,
 													Keywords = keywords,
 													Reward = pay,
-													AssignmentDurationInSeconds = 60 * 60, # 60 minutes
+													AssignmentDurationInSeconds = 60 * 60, # 30 minutes
 													AutoApprovalDelayInSeconds = 2* 24 * 60 * 60, # the number of seconds after an assignment is submitted is it automatically approved unless explicitly rejected
 	                        ## the norm is to try to keep this under 7 days, many requesters approve in less than 3 days
 	                        RequesterAnnotation = description,
